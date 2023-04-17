a09ca5d595b66836cc59df96fbe1f059b1f1264e
Нисик <@DESKTOP-ALORV4C>
2023-04-17 15:50:24 +0300

    Добавьте файлы проекта.

---
+++ day 1 (9)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 16.55e-3;
+        double y = -2.75;
+        double z = 0.15;
+
+        double b = Math.Sqrt(10) * Math.Pow(Math.Pow(x, 3) + Math.Pow(x, y) + 2, 0.5)
+                   * (Math.Pow(Math.Asin(z), 2) - x - y);
+
+        Console.WriteLine(b); // -40.63069
+    }
+}
